a53dd08d5ce678dcd792b31f47000d2c3f575793
Matheus Paice <paice.zinner@gmail.com>
2024-12-23 08:25:35 -0300

    hotfix: correct domain images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['vitruveo-studio-qa-assets.s3.amazonaws.com'],
+        domains: ['vitruveo-studio-qa-assets.s3.amazonaws.com', 'vitruveo-studio-production-assets.s3.amazonaws.com'],
     },
     async redirects() {
         return [

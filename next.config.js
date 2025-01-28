7d4ac4b9c3ea9460ce4efe421c5d8f0bed1570eb
marcos <marcos.barboza@jbtec.com.br>
2025-01-28 18:00:41 -0300

    fix: menuItems

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['vitruveo-studio-qa-assets.s3.amazonaws.com', 'vitruveo-studio-production-assets.s3.amazonaws.com'],
+        domains: [
+            'vitruveo-studio-dev-assets.s3.amazonaws.com',
+            'vitruveo-studio-qa-assets.s3.amazonaws.com',
+            'vitruveo-studio-production-assets.s3.amazonaws.com',
+        ],
     },
     async redirects() {
         return [

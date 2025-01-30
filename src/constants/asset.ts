660e560627c7e0c23abdcc33e28878134b14b189
marcos <marcos.barboza@jbtec.com.br>
2025-01-30 13:58:59 -0300

    fix: constants

--- src/constants/asset.ts
+++ src/constants/asset.ts
@@ -5,4 +5,4 @@ export const GENERAL_STORAGE_URL =
     process.env.NEXT_PUBLIC_GENERAL_STORAGE_URL || 'https://vitruveo-studio-dev-general.s3.amazonaws.com';
 
 export const STORE_STORAGE_URL =
-    process.env.NEXT_PUBLIC_STORE_STORAGE_URL || 'https://vitruveo-studio-dev-stores.s3.amazonaws.com';
+    process.env.NEXT_PUBLIC_STORES_STORAGE_URL || 'https://vitruveo-studio-dev-stores.s3.amazonaws.com';

496f595129c895661b87d370acddafcc5e908cad
Luan Almeida <luan.almeida@jbtec.com.br>
2025-02-24 11:00:31 -0300

    feat: increase preview size and invert filter and preview order

--- src/app/home/components/apps/stores/review/index.tsx
+++ src/app/home/components/apps/stores/review/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { SelectedFilter } from './selectedFilterSection';
 import { Artworks } from '@/features/stores/types';
 
@@ -8,14 +8,12 @@ interface Props {
 export const Review = ({ values }: Props) => {
     return (
         <Box>
-            <Card sx={{ padding: 2 }}>
-                <Grid container spacing={4}>
-                    {Object.entries(values).map((element) => {
-                        const [key, value] = element;
-                        return <SelectedFilter key={key} title={key} content={value} />;
-                    })}
-                </Grid>
-            </Card>
+            <Grid container spacing={4}>
+                {Object.entries(values).map((element) => {
+                    const [key, value] = element;
+                    return <SelectedFilter key={key} title={key} content={value} />;
+                })}
+            </Grid>
         </Box>
     );
 };
